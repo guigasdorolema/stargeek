24fbee42d73f0795da279443e537299d2901131e
Guilherme Silva Cardozo de Oliveira <guilherme.cardozo@portalsesisp.org.br>
2023-05-16 14:30:22 -0300

    3

--- cadastro.js
+++ cadastro.js
@@ -3,7 +3,7 @@ const email = document.getElementById("Email");
 const senha= document.getElementById("Senha");
 const nome= document.getElementById("Nome");
 const formulario = document.getElementById("formulario");
-
+alert("Cadastro Feito")
 
 formulario.onsubmit = (evento) =>{
     let dados = JSON.parse(localStorage.getItem("dados"));
