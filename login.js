264820d2a99ed28312c0fac295406f694e2a6769
Guilherme Silva Cardozo de Oliveira <guilherme.cardozo@portalsesisp.org.br>
2023-05-29 13:34:20 -0300

    s

--- login.js
+++ login.js
@@ -1,22 +1,35 @@
 const mensagem = document.querySelector(".mensagem");
-const email = document.getElementById("Email");
-const senha= document.getElementById("Senha");
-const nome= document.getElementById("Nome");
 const formulario = document.getElementById("formulario");
-
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
 
 formulario.onsubmit = (evento) =>{
-    let dados = JSON.parse(localStorage.getItem("dados"));
 
-    dados.forEach(element => {
-        if (MediaElementAudioSourceNode.email == email.value && MediaElementAudioSourceNode.senha == senha.value) {
-            evento.preventDefault();
-            mensagem.innerHTML="Logado";
-            return true;
-        } else{
-            evento.preventDefault();
-            mensagem.innerHTML ="Senha ou E-mail Incorreto";
-        }
-        
-    });
+    let dados= JSON.parse(localStorage.getItem("dados"));
+    let logado;
+   
+   dados.forEach(element => {
+    if (element.email == email.value && element.senha == senha.value)
+    {
+        mensagem.innerHTML= "aguarde..."
+        evento.preventDefault();
+          let dados = JSON.parse(sessionStorage.getItem("logado")) ||
+            dados.push(
+             {
+                  email: email.value
+             }
+
+            )
+        sessionStorage.setItem("logado", JSON.stringify(dados));
+        setTimeout(()=> {
+            window.location.assign("cadastro.html")
+        },3000)
+        return true;
+    }
+    else{
+        evento.preventDefault();
+        mensagem.innerHTML="senha ou email incorreto";
+    }
+   
+   });
 }
